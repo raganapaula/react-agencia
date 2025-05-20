cc0a33fb6c73eb141ebe1a4471f69d807b7b0a23
Ana Paula <119074534+raganapaula@users.noreply.github.com>
2025-05-20 14:06:08 -0300

    Ajuste de imagens na Gallery

--- app/components/ImgGallery.tsx
+++ app/components/ImgGallery.tsx
@@ -4,62 +4,62 @@ export default function ImgGallery(){
       className="grid p-10 gap-2 justify-items-center grid-cols-2 lg:grid-cols-4"
     >
       <img
-        src="img gallery/gallery-1.jpg.svg"
+        src="img/img gallery/gallery-1.jpg.svg"
         alt="Gallery Image 1"
         className="w-70 h-auto mb-4"
       />
       <img
-        src="img gallery/gallery-2.jpg.svg"
+        src="img/img gallery/gallery-2.jpg.svg"
         alt="Gallery Image 2"
         className="w-70 h-auto mb-4"
       />
       <img
-        src="img gallery/gallery-3.jpg.svg"
+        src="img/img gallery/gallery-3.jpg.svg"
         alt="Gallery Image 3"
         className="w-70 h-auto mb-4"
       />
       <img
-        src="img gallery/gallery-4.jpg.svg"
+        src="img/img gallery/gallery-4.jpg.svg"
         alt="Gallery Image 4"
         className="w-70 h-auto mb-4"
       />
       <img
-        src="img gallery/gallery-5.jpg.svg"
+        src="img/img gallery/gallery-5.jpg.svg"
         alt="Gallery Image 5"
         className="w-70 h-auto mb-4"
       />
       <img
-        src="img gallery/gallery-6.jpg.svg"
+        src="img/img gallery/gallery-6.jpg.svg"
         alt="Gallery Image 6"
         className="w-70 h-auto mb-4"
       />
       <img
-        src="img gallery/gallery-7.jpg.svg"
+        src="img/img gallery/gallery-7.jpg.svg"
         alt="Gallery Image 7"
         className="w-70 h-auto mb-4"
       />
       <img
-        src="img gallery/gallery-8.jpg.svg"
+        src="img/img gallery/gallery-8.jpg.svg"
         alt="Gallery Image 8"
         className="w-70 h-auto mb-4"
       />
       <img
-        src="img gallery/gallery-9.jpg.svg"
+        src="img/img gallery/allery-9.jpg.svg"
         alt="Gallery Image 9"
         className="w-70 h-auto mb-4"
       />
       <img
-        src="img gallery/gallery-10.jpg.svg"
+        src="img/img gallery/gallery-10.jpg.svg"
         alt="Gallery Image 10"
         className="w-70 h-auto mb-4"
       />
       <img
-        src="img gallery/gallery-11.jpg.svg"
+        src="img/img gallery/gallery-11.jpg.svg"
         alt="Gallery Image 11"
         className="w-70 h-auto mb-4"
       />
       <img
-        src="img gallery/gallery-12.jpg.svg"
+        src="img/img gallery/gallery-12.jpg.svg"
         alt="Gallery Image 12"
         className="w-70 h-auto mb-4"
       />
