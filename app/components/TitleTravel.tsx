2f6a98bf20beefd05ade865140973b156c7e7650
Ana Paula <119074534+raganapaula@users.noreply.github.com>
2025-05-20 11:03:22 -0300

    Criando Title Travel

---
+++ app/components/TitleTravel.tsx
@@ -0,0 +1,15 @@
+export default function TitleTravel(){
+    return(
+        <div className="flex flex-col items-center mt-5">
+      <span className="text-orange-500 font-kaushan text-[2rem] italic">Choose Your</span>
+      <span className="text-black font-poppins text-[2rem]">Perfect Holiday</span>
+      
+      <div className="w-12 h-0.5 bg-orange-500 my-2"></div>
+      <span className="text-black font-poppins text-[0.8rem]">The most searched holiday destinations</span>
+    </div>
+
+
+
+
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import TitleTravel from "./components/TitleTravel";
 
 
 export default function Home() {
 return(
  <div>
  <Header/>
+ <TitleTravel/>
  </div>
 )
 }
